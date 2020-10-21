6f7efdf6e5efdca2c7485ffa719d3ce7f1fd5b74
ssrout <smruti.sambit.rout@gmail.com>
2020-10-21 21:16:27 +0530

    reactive form validation added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { SharedModule } from './shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +48,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { AccountService } from './../_services/account.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,35 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister=new EventEmitter();
   model:any={};
+  registerForm:FormGroup;
   constructor(private accountService:AccountService,private toastr:ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){//reactive form
+    this.registerForm=new FormGroup({
+      username:new FormControl('',Validators.required),
+      password:new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword:new FormControl('',[Validators.required,this.matchValues('password')])
+    })
+  }
+
+  matchValues(match:string):ValidatorFn{//custom validator
+    return(control:AbstractControl)=>{
+      return control?.value===control?.parent?.controls[match].value?null:{isMatch:true}
+    }
   }
 
   register(){
-   this.accountService.register(this.model).subscribe(response=>{
-     this.cancel();
-   },error=>{
-     console.log(error);
-     this.toastr.error(error.error);
-   });
+    console.log(this.registerForm.value);
+  //  this.accountService.register(this.model).subscribe(response=>{
+  //    this.cancel();
+  //  },error=>{
+  //    console.log(error);
+  //    this.toastr.error(error.error);
+  //  });
   }
 
   cancel(){
